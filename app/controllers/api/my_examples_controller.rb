5f6ef89bc62090175775d64e5abeeb2509e80836
myalisa <myalisamiro@gmail.com>
2019-08-27 11:26:48 -0500

    Final_Fortune_app

--- app/controllers/api/my_examples_controller.rb
+++ app/controllers/api/my_examples_controller.rb
@@ -1,10 +1,36 @@
 class Api::MyExamplesController < ApplicationController
-def fortune_action
-    render json: {message: "You will have a great day"}
+  def fortune_action
+    
+      fortunes = ["You will face trouble in work",
+
+               "A great opportunity will present itself to you", 
+
+               "Never underestimate the value of the present moment"
+             ]
+      random_number = rand(0..2)
+      @fortune = fortunes[random_number]
+
+
+      render "fortune_view.json.jb"
+    end
+    def random_action
+      possible_numbers = (1..60).to_a
+      @randoms = []
+
+      6.times do
+        possible_numbers.shuffle!
+        picked_number = possible_numbers.pop
+        @randoms << picked_number
+      end
+
+      render "random_view.json.jb"
+    end
+
+  def refresh_action
+    @count += 1
+    render 'refresh_view.json.jb'
   end
 
-  def random_action
-    
-    render 
-  end
+
+
 end
